add940136a04bfaaaf5dfe7130a6b7cdb33b06e4
Milad Bannourah <72631935+miladBann@users.noreply.github.com>
2023-07-28 22:27:30 +0300

    Update experienceConfig.js

--- src/config/resume/experienceConfig.js
+++ src/config/resume/experienceConfig.js
@@ -1,41 +1,51 @@
 export const experienceConfig = [
   {
-    name: "Chrono Group, LLC",
+    name: "Skinstric",
     title: "Remote Frontend Developer",
     description: [
-      `Assisted in developing various projects using AWS services (Route53, CloudFront, S3), and maintained 20+
-      web pages, contributing to the development, testing, and updates of 5 WordPress sites.
-      `,
-      `Led the creation and maintenance of front-end code for numerous e-Commerce sites, managing site
-      redesigns, seasonal updates, and production work.
-      `,
-      `Collaboratively resolved 120+ software bugs and conducted regular updates throughout production
-      lifecycles, developing numerous user-facing features via reusable code and libraries.`,
-      `Ensured the technical feasibility and optimization of 150+ UI/UX designs, resulting in maximized speed and
-      scalability for 18+ applications.
-      `
+      "Optimize frontend code for 40% improvement in performance and faster loading times.",
+      "Conduct thorough testing of frontend code, swiftly identifying and resolving 80+ bugs or issues.",
+      "Collaborated with the backend team to integrate frontend functionalities with AI algorithms through API calls.",
+      "Transformed 14 design mockups into visually appealing, functional user interfaces using JavaScript and React.",
+      "Implement user input forms for skin type, concerns, and preferences. Handle data validation and ensure secure data transmission to the backend."
     ],
-    date: "Feb 2020 - Current",
+    date: "Feb 2022 - Current",
   },
   {
-    name: "Frontend Developer Internship",
-    title: "Frontend Simplified",
+    name: "Chrono Group, LLC",
+    title: "Remote Full Stack Developer ",
     description: [
-      "Transformed a completely static HTML, CSS, JavaScript and React single page application into an interactive user interface using animations, transitions and carousels.",
-      "Processed API requests with Axios to dynamically represent data from a cloud server and represented it through skeleton loading states, pagination and dynamic routing.",
-      "Utilized Git version control and the GitHub interface to work and thrive in a virtual and collaborative team environment",
+      "Orchestrated diverse AWS projects (Route53, CloudFront, S3) for 20+ web pages and 5 WordPress sites.",
+      "Collaboratively remediated 120+ software anomalies, contributing to user-centric features via reusable code.",
+      "Verified and optimized feasibility of 150+ UI/UX designs for 18+ applications.", 
+      "Implementing security measures such as encryption, authentication, and authorization.",
+      "Optimizing backend code and database queries to improve application performance and response times."
+      
     ],
-    date: "Dec 2019 - Feb 2020",
+    date: "Jan 2020 - Feb 2022",
+  },
+  {
+    name: "Qoves",
+    title: "Full Stack Developer Intern ",
+    description: [
+      "Designed and managed 4 databases to store and retrieve application data efficiently.",
+      "Created API endpoints to handle various operations: creating, reading, updating, and deleting from the database.",
+      "Transformed 6 static HTML, CSS, JS, and React SPA into an interactive UI with animations and carousels.",
+      "Processed API requests using Axios to show dynamic data, skeleton loading, pagination, and dynamic routing.",
+      "Excelled in Git version control and GitHub collaboration, thriving in virtual team environments."
+    ],
+    date: "July 2019 - Jan 2020",
   },
   {
     name: "Freelance Full-Stack Developer",
     title: "self employed",
     description: [
-      "Built an Ecommerce website for an electronics store in Jerusalem called Seven7 which resulted in 40% increase in online sales and many more websites, desktop apps, web apps using various technologies.",
-      "Improved website performance by compressing, caching and lazy loading images and components.",
-      "Included SEO, responsive, multi-platform and hosting support, enabling their website to be used everywhere.",
-      "Mentored and helped Students and developers in enhancing their Full-Stack apps performance and design."
+      "Built an Ecommerce website for an electronics store in Jerusalem called Seven7 which resulted in 40% increase in online sales",
+      "Built 55+ websites, desktop apps, web apps using Python, JavaScript, TypeScript, React, Next.js, Node.js, AWS and many more.",
+      "Improved 28 website performance by compressing, caching and lazy loading images and components.",
+      "Included SEO, responsive, multi-platform and hosting support, enabling the websites to be used everywhere.",
+      "Helped 1000+ Students and developers in enhancing their Full-Stack apps performance and design."
     ],
-    date: "July 2019 - Feb 2023",
+    date: "Jan 2018 - Feb 2023",
   },
 ];
